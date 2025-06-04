ce0e7cb2116a6e7996aec7c77e745f8ccf08b115
Liam Edward Brem <liambrem@meta.com>
2025-06-04 10:20:19 -0700

    Modal appearing

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,41 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".likes").forEach(likeSection => {
+        likeSection.addEventListener("click", () => {
+            const icon = likeSection.querySelector(".like-icon");
+            const countSpan = likeSection.querySelector(".like-count");
+            let count = parseInt(countSpan.textContent);
+
+            if (icon.classList.contains("liked")) {
+                icon.classList.remove("fa-solid", "liked");
+                icon.classList.add("fa-regular");
+                countSpan.textContent = count - 1;
+            } else {
+                icon.classList.remove("fa-regular");
+                icon.classList.add("fa-solid", "liked");
+                countSpan.textContent = count + 1;
+            }
+        });
+    });
+});
+
+
+// JavaScript for Opening and Closing the Modal
+const modal = document.getElementById("PlaylistModal");
+const span = document.getElementsByClassName("close")[0];
+
+function openModal(playlist) {
+   document.getElementById('PlaylistTitle').innerText = playlist.name;
+   document.getElementById('PlaylistImage').src = playlist.imageUrl;
+   document.getElementById('CreatorName').innerText = playlist.creatorName;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
